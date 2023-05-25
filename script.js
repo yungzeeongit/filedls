919f13fc9e9a91bb0256b3096d46d5d7a03da271
yungzeeongit <greyw4300@gmail.com>
2023-05-25 08:34:43 -0700

    code

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const fileInput = document.querySelector("input")
 
+const downloadBtn = document.querySelector("button")
+
+
+downloadBtn.addEventListener('click',e =>{
+    // e.preventDefault();
+   fetchFile(fileInput.value)
+})
+
+function fetchFile(url){
+    fetch(url).then(res => res.blob()).then(file =>{
+        console.log(file)
+    })
+}
