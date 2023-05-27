e5c4e1aaa7299bbe5e37f92007ad2e7433051fb2
yungzeeongit <greyw4300@gmail.com>
2023-05-26 23:04:20 -0700

    progress made

--- script.js
+++ script.js
@@ -6,10 +6,23 @@ const downloadBtn = document.querySelector("button")
 downloadBtn.addEventListener('click',e =>{
     // e.preventDefault();
    fetchFile(fileInput.value)
+   downloadBtn.innerText = "Downloading file"
 })
 
 function fetchFile(url){
     fetch(url).then(res => res.blob()).then(file =>{
-        console.log(file)
+        let tempUrl = URL.createObjectURL(file)
+        let aTag = document.createElement("a")
+        aTag.href = tempUrl
+        aTag.download = url.replace(/^.*[\\\/]/, '');
+        document.body.appendChild(aTag)
+        aTag.click()
+        aTag.remove()
+        URL.revokeObjectURL(tempUrl) //
+        downloadBtn.innerText = "Download file"
+        // console.log(file)
+    }).catch(() => {
+        downloadBtn.innerText = "Download file";
+        alert("Error downloading")
     })
 }
